5eaed5db48b5d08eb928b3c89c56c69a0ee4cbd2
DaniswaraEkaS <vdanniz@gmail.com>
2025-01-23 16:41:22 +0700

    update almost done

--- airflow/scripts/transform.py
+++ airflow/scripts/transform.py
@@ -7,25 +7,26 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
 
 spark = SparkSession.builder.getOrCreate()
+# Initialize SentimentIntensityAnalyzer
+sia = SentimentIntensityAnalyzer()
 
 
+# Define a UDF for sentiment score
+def sentiment_score_udf(text):
+    if text:
+        return sia.polarity_scores(text)['compound']
+    return 0.0
+
+# Define a UDF for sentiment label
+def sentiment_label_udf(score):
+    if score > 0.05:
+        return 'Positive'
+    elif score < -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
+    
 def add_sentiment_analysis(data1):
-    # Initialize SentimentIntensityAnalyzer
-    sia = SentimentIntensityAnalyzer()
-    # Define a UDF for sentiment score
-    def sentiment_score_udf(text):
-        if text:
-            return sia.polarity_scores(text)['compound']
-        return 0.0
-    # Define a UDF for sentiment label
-    def sentiment_label_udf(score):
-        if score > 0.05:
-            return 'Positive'
-        elif score < -0.05:
-            return 'Negative'
-        else:
-            return 'Neutral'
-
     # Register UDFs
     sentiment_score = udf(sentiment_score_udf, FloatType())
     sentiment_label = udf(sentiment_label_udf, StringType())
@@ -36,9 +37,14 @@ def add_sentiment_analysis(data1):
     
     return data1
 
+def add_month_year_column_data1(data1, created_time_column="created_time", month_year_column="month_year"):
+    
+    data1 = data1.withColumn(month_year_column, date_format(to_date(created_time_column), "yyyy-MM"))
+    
+    return data1
 
 
-def add_month_year_column(data2):
+def add_month_year_column_data2(data2):
     data2 = data2.withColumn(
         "month_num",
         when(col("Month") == "January", "01")
@@ -60,32 +66,9 @@ def add_month_year_column(data2):
     return data2
 
 
-
-def data_cleaning(data1, data2):
-    #handling missing value in data1
-    data1_cleaned = data1.na.drop(subset=['post_id'])
-    data1_cleaned = data1_cleaned.na.drop(subset=['post_title'])
-    data1_cleaned = data1_cleaned.na.drop(subset=['comment_id'])
-    data1_cleaned = data1_cleaned.na.drop(subset=['self_text'])
-    data1_cleaned = data1_cleaned.na.drop(subset=['subreddit'])
-    data1_cleaned = data1_cleaned.na.drop(subset=['author_name'])
-
-    # kalau user is verified null maka isi jadi False
-    #
-    
-    #handling missing value in data2
-    data2_cleaned = data2.na.drop(subset=['country'])
-    data2_cleaned = data2_cleaned.na.drop(subset=['month_year'])
-    
-    #handling duplicates
-    data1_cleaned.dropDuplicates()
-    data2_cleaned.dropDuplicates()
-    
-    return data1_cleaned, data2_cleaned
-
 def add_date_table():
     # Define start and end dates
-    start_date = datetime(2023, 9, 2)
+    start_date = datetime(2016, 1, 1)
     end_date = datetime(2025, 1, 18)
 
     # Generate list of distinct dates
@@ -98,11 +81,38 @@ def add_date_table():
     # Add month, year, and month_year columns with numeric month
     date_df = date_df.withColumn("date", col("date").cast(DateType())) \
         .withColumn("month", date_format(col("date"), "MM")) \
-        .withColumn("year", date_format(col("date"), "yyyy"))
+        .withColumn("year", date_format(col("date"), "yyyy"))\
+        .withColumn("month_year", concat_ws("-", date_format(col("date"), "MM"), date_format(col("date"), "yyyy")))    
 
     return date_df
 
+def data_cleaning(data1, data2, date_df):
+    # Cleaning for data1
+    # handling missing value in data1
+    data1_cleaned = data1.na.drop(subset=['post_id', 'post_title', 'comment_id', 'self_text', 'subreddit', 
+                                        'author_name', 'month_year', 'sentiment_score', 'sentiment_label'])
+    # kalau user is verified null maka isi jadi False
+    data1_cleaned = data1_cleaned.fillna({'user_is_verified': False})
+    data1_cleaned.dropDuplicates()
+    
+    # Cleaning for data2
+    # renamed column names
+    data2_cleaned = data2.withColumnRenamed('Country','country')\
+            .withColumnRenamed('Events','events')\
+            .withColumnRenamed('Fatalities','Fatalities')\
+                
+    # handling missing value in data2
+    data2_cleaned = data2_cleaned.na.drop(subset=['country','month_year'])
+    data2_cleaned = data2_cleaned.fillna({'events': 0, 'fatalities': 0})
+    # handling duplicates
+    data2_cleaned.dropDuplicates()
+    
+    # Cleaning for date_df
 
+    date_df_cleaned = date_df.na.drop(subset=['date','month_year'])
+    date_df_cleaned.dropDuplicates()
+    
+    return data1_cleaned, data2_cleaned, date_df_cleaned
 
 
 
@@ -116,33 +126,33 @@ if __name__ == '__main__':
 
     # Download the required lexicon
     nltk.download('vader_lexicon')
-
+    
     # Tambahkan hasil sentiment analysis ke data1
     data1 = add_sentiment_analysis(data1)
+    
+    # Menambah kolom month_year ke data1
+    data1 = add_month_year_column_data1(data1, created_time_column="created_time", month_year_column="month_year")
 
     # Tambahkan kolom month_year ke data2
-    data2 = add_month_year_column(data2)
+    data2 = add_month_year_column_data2(data2)
 
+    # membuat date table
     date_df = add_date_table()
-
-    fact_comment_columns = data1.select("comment_id", "self_text", "subreddit","created_time",
+    
+    # Cleaning data
+    data1_cleaned, data2_cleaned, date_df_cleaned = data_cleaning(data1, data2, date_df)
+    # memasukkan kolom yang mau dijadikan fact & dim table
+    fact_comment_columns = data1_cleaned.select("comment_id", "self_text", "subreddit","created_time",
                                         "controversiality","score","author_name","post_id", "sentiment_score", "sentiment_label")
-    fact_assault_columns = data2.select("country", "month_year", "events", "fatalities")
-    dim_user_columns = data1.select("author_name", "user_is_verified", "user_account_created_time", "user_awardee_karma", 
-                                    "user_awarder_karma", "user_link_karma", "user_comment_karma", "user_total_karma") #tolong benerin karena ini mengakibatkan table dim user tidak unik untuksetiap user
-    dim_post_columns = data1.select("post_id", "post_score", "post_title", "post_self_text", "post_upvote_ratio", 
-                                    "post_thumbs_ups", "post_total_awards_received", "post_created_time", "month_year") #idempoten with dim user problem
-    dim_date_columns = date_df.select("date", "month", "year")
-    # dim_date_with_month_year = fact_assault_columns.crossjoin(fact_assault_columns.select("month_year").distinct())
-
+    fact_assault_columns = data2_cleaned.select("country", "month_year", "events", "fatalities")
+    dim_user_columns = data1_cleaned.select("author_name", "user_is_verified", "user_account_created_time", "user_awardee_karma", 
+                                    "user_awarder_karma", "user_link_karma", "user_comment_karma", "user_total_karma").distinct() 
+    dim_post_columns = data1_cleaned.select("post_id", "post_score", "post_title", "post_self_text", "post_upvote_ratio", 
+                                    "post_thumbs_ups", "post_total_awards_received", "post_created_time", "month_year").distinct() 
+    dim_date_columns = date_df_cleaned.select("date", "month", "year", "month_year")
     # Select columns for each new DataFrame
     fact_comment_table = data1.select(*fact_comment_columns)
     fact_assault_table = data2.select(*fact_assault_columns)
     dim_user_table = data1.select(*dim_user_columns)
     dim_post_table = data1.select(*dim_post_columns)
     dim_date_table = date_df.select(*dim_date_columns)
-    # fact_comment_table
-    # fact_assault_table
-    # dim_user_table
-    # dim_post_table
-    # dim_date_table
